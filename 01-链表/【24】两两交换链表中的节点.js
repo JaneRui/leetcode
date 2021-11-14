539506fadcb9a29f226d80d3fa9a6bb0b63b0404
JaneRui <janerui8@163.com>
2021-11-14 15:24:51 +0800

    【24】两两交换链表中的节点-虚拟头节点

--- 01-链表/【24】两两交换链表中的节点.js
+++ 01-链表/【24】两两交换链表中的节点.js
@@ -9,7 +9,7 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
- var swapPairs = function(head) {
+var swapPairs1 = function(head) {
   // 边界条件处理
   if (!head) return head;
   if (head && !head.next) return head;
@@ -19,4 +19,24 @@
   headNext.next = head;
   head.next = subResult;
   return headNext;
+};
+
+var swapPairs = function(head) {
+  // 边界条件处理
+  if (!head) return head;
+  if (head && !head.next) return head;
+  // 两两交换
+  var dumpy = new ListNode(null, head);
+  var pre = dumpy, tail = dumpy, temp = dumpy;
+  while (temp.next && temp.next.next) { // 一组一组进行交换
+    pre = pre.next;
+    tail = pre.next; // tail一直是pre的下一个节点
+    // 重新设置两个节点的指向
+    [pre.next, tail.next] = [tail.next, pre];
+    // 重新让头指针指向tail
+    temp.next = tail;
+    // 将temp移动到上一组翻转后的尾节点
+    temp = pre;
+  }
+  return dumpy.next;
 };
